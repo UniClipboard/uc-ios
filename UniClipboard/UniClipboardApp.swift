8360c6b658707f5db3ae075421483cad24f47859
mkdir700 <mkdir700@gmail.com>
2026-05-09 07:50:55 +0800

    Initial Commit

---
+++ UniClipboard/UniClipboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniClipboardApp.swift
+//  UniClipboard
+//
+//  Created by mark on 2026/5/9.
+//
+
+import SwiftUI
+
+@main
+struct UniClipboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
